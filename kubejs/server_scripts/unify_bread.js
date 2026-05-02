8cbe2b62f6a6947cf8385d196c980f711a7e2eb4
stann-co <jackconradsen@gmail.com>
2026-05-02 14:46:50 +0200

    attempt to fix bread

---
+++ kubejs/server_scripts/unify_bread.js
@@ -0,0 +1,30 @@
+ServerEvents.recipes(event => {
+    // 1. Replace inputs
+    event.replaceInput(
+        {},
+        'farmersdelight_extended:bread_slice',
+        'someassemblyrequired:bread_slice' 
+    )
+    
+    event.replaceInput(
+        {},
+        'farmersdelight_extended:toast', 
+        'someassemblyrequired:toasted_bread_slice'
+    ) 
+    
+    // 2. Replace outputs
+    event.replaceOutput( 
+        {}, 
+        'farmersdelight_extended:bread_slice', 
+        'someassemblyrequired:bread_slice' 
+    ) 
+    
+    event.replaceOutput( 
+        {}, 
+        'farmersdelight_extended:toast', 
+        'someassemblyrequired:toasted_bread_slice' 
+    ) 
+    
+    // 3. Remove recipes 
+    event.remove({ output: 'farmersdelight_extended:bread_slice' })
+    event.remove({ output: 'farmersdelight_extended:toast' }) })
